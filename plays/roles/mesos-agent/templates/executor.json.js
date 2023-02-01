f3f90149fff9e4b7234b5f3a33459925a81f702f
Andreas Peters <ap@aventer.biz>
2023-02-01 23:39:14 +0100

    ADD: support to configure only new workers.

--- plays/roles/mesos-agent/templates/executor.json.js
+++ plays/roles/mesos-agent/templates/executor.json.js
@@ -1,5 +1,6 @@
 {
     "LIBPROCESS_SSL_ENABLED" : "{{ libprocess_enable_ssl }}",
+    "LIBPROCESS_SSL_ENABLE_TLS_V1_3" : "{{ libprocess_enable_ssl }}",
     "LIBPROCESS_SSL_SUPPORT_DOWNGRADE": "{{ libprocess_ssl_enable_downgrade }}",
     "LIBPROCESS_SSL_VERIFY_CERT" : "{{ libprocess_enable_ssl_verify }}",
     "LIBPROCESS_SSL_REQUIRE_CERT" : "{{ libprocess_ssl_require_cert }}",
