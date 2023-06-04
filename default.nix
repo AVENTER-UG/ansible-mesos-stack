66d66832b28343ebdd36d9a78c35f588baaf4206
Andreas Peters <ap@aventer.biz>
2023-06-04 23:07:42 +0200

    ADD: dhclient config.

--- default.nix
+++ default.nix
@@ -4,9 +4,21 @@ with pkgs;
 
 mkShell {
   buildInputs = [
+    python310
+    python310Packages.pip
+    python310Packages.virtualenv
     ansible
     openssh
     sshpass
+    jq
   ];
+
+  shellHook = ''
+    [ ! -d '$PROJDIR/python-dev' ] && virtualenv python-dev && echo "SETUP python-dev: DONE"
+    source python-dev/bin/activate
+    pip install boto3
+    export PATH=/home/andreas/bin:$PATH
+  '';
+  
 }
 
